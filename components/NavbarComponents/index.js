c0dcb059873bf87edeb87756bad42f1cd1ec5025
kallyas <kallyasmedia@gmail.com>
2022-10-03 03:20:33 +0300

    ch: create navbar components

---
+++ components/NavbarComponents/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./NavbarComponents.module.css";
+import Link from "next/link";
+import { FaGithub, FaTwitter } from "react-icons/fa";
+
+export const socialIcons = [<FaGithub />, <FaTwitter />];
+
+export const Logo = (
+  <Link href="/">
+    <a className={styles.logo}>Kallyas</a>
+  </Link>
+);
+
+export const navItems = [
+  {
+    name: "Home",
+    id: "/",
+  },
+  {
+    name: "About",
+    id: "about",
+  },
+  {
+    name: "Projects",
+    id: "portfolio",
+  },
+  {
+    name: "FAQ",
+    id: "faq",
+  },
+  {
+    name: "Appointment",
+    id: "appointment",
+  },
+];
